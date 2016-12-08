81b2a44a901cc940a8b0d4ab82a6b00eaded5277
SunnySinghs <sourabh.singh000@gmail.com>
2016-12-08 22:53:38 +0530

    First Commit

---
+++ Testing/src/com/Test.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.sql.Date;
+import java.util.GregorianCalendar;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+import com.hibernate.chapter1.Student;
+
+public class Test {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+}
+}
